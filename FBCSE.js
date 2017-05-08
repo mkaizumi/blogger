50806cbdddd688610246d312416cf117566a222a
mkaizumi <okezumi@yahoo.com>
2017-05-08 16:43:12 +0800

    Create FBCSE.js

---
+++ FBCSE.js
@@ -0,0 +1,10 @@
+// facebook
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=142594145757478";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+ //search RC&CT
+ !function(){var t="partner-pub-4148776271381849:9519161216",e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=("https:"==document.location.protocol?"https:":"http:")+"//cse.google.com/cse.js?cx="+t;var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(e,c)}();
